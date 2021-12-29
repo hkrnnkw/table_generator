d85ade80b90b402851cdeb5de9dd2c2f9e9ea650
hkrnnkw <4901108@gmail.com>
2021-12-30 00:13:01 +0900

    feat: create rects based on initialTable

--- src/plugin/controller.ts
+++ src/plugin/controller.ts
@@ -1,15 +1,25 @@
+import { TableType } from '../utils/types';
+
 figma.showUI(__html__);
 
 figma.ui.onmessage = (msg) => {
   if (msg.type === 'create-table') {
-    const nodes = [];
+    const table: TableType = msg.currentTable;
+    const nodes: RectangleNode[] = [];
 
-    for (let i = 0; i < msg.count; i++) {
-      const rect = figma.createRectangle();
-      rect.x = i * 150;
-      rect.fills = [{ type: 'SOLID', color: { r: 1, g: 0.5, b: 0 } }];
-      figma.currentPage.appendChild(rect);
-      nodes.push(rect);
+    for (let i = 0; i < table.rows.length; i++) {
+      const row = table.rows[i];
+      for (let j = 0; j < table.columns.length; j++) {
+        const rect = figma.createRectangle();
+        const columnId = row.cells[j].columnId;
+        const column = table.columns.find((column) => column.id === columnId);
+        rect.x = j * column.width;
+        rect.y = i * row.height;
+        rect.resize(column.width, row.height);
+        rect.fills = [{ type: 'SOLID', color: { r: 1, g: 0.5, b: 0 } }];
+        figma.currentPage.appendChild(rect);
+        nodes.push(rect);
+      }
     }
 
     figma.currentPage.selection = nodes;
@@ -18,7 +28,7 @@ figma.ui.onmessage = (msg) => {
     // This is how figma responds back to the ui
     figma.ui.postMessage({
       type: 'create-table',
-      message: `Created ${msg.count} Rectangles`,
+      message: `Created Table: its id is ${table.id}`,
     });
   }
 };
