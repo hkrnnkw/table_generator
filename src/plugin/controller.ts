58e8482279481f302db264c876d4e9d3d7d1b279
hkrnnkw <4901108@gmail.com>
2021-12-30 11:42:07 +0900

    create cell group
    text and rect in the group

--- src/plugin/controller.ts
+++ src/plugin/controller.ts
@@ -6,15 +6,19 @@ figma.showUI(__html__);
 figma.ui.onmessage = (msg) => {
   if (msg.type === 'create-table') {
     const table: TableType = msg.currentTable;
-    const frame = createTableFrame(table);
-    figma.currentPage.appendChild(frame);
-    figma.currentPage.selection = [frame];
-    figma.viewport.scrollAndZoomIntoView([frame]);
-
-    // This is how figma responds back to the ui
-    figma.ui.postMessage({
-      type: 'create-table',
-      message: `Created Table: its id is ${table.id}`,
-    });
+    createTableFrame(table)
+      .then((frame) => {
+        figma.currentPage.appendChild(frame);
+        figma.currentPage.selection = [frame];
+        figma.viewport.scrollAndZoomIntoView([frame]);
+      })
+      .then(() => {
+        // This is how figma responds back to the ui
+        figma.ui.postMessage({
+          type: 'create-table',
+          message: `Created Table: its id is ${table.id}`,
+        });
+      })
+      .catch((err) => console.error(err));
   }
 };
--- src/plugin/handlers/tableRenderer.ts
+++ src/plugin/handlers/tableRenderer.ts
@@ -1,6 +1,40 @@
-import { TableType } from '../../utils/types';
+import { TableCellType, TableType } from '../../utils/types';
 
-export const createTableFrame = (table: TableType): FrameNode => {
+const createCellText = async (
+  cell: TableCellType,
+  columnWidth: number,
+  rowHeight: number,
+  x: number,
+  y: number
+): Promise<TextNode> => {
+  await figma.loadFontAsync({ family: 'Roboto', style: 'Regular' });
+  const text = figma.createText();
+  text.x = x * columnWidth;
+  text.y = y * rowHeight;
+  text.resize(columnWidth, rowHeight);
+  text.characters = cell.text;
+  text.strokes = [{ type: 'SOLID', color: cell.color }];
+  return text;
+};
+
+const createCellRect = (
+  cell: TableCellType,
+  columnWidth: number,
+  rowHeight: number,
+  x: number,
+  y: number
+): RectangleNode => {
+  const rect = figma.createRectangle();
+  rect.x = x * columnWidth;
+  rect.y = y * rowHeight;
+  rect.resize(columnWidth, rowHeight);
+  rect.fills = [{ type: 'SOLID', color: cell.backgroundColor }];
+  return rect;
+};
+
+export const createTableFrame = async (
+  table: TableType
+): Promise<FrameNode> => {
   const frame = figma.createFrame();
 
   for (let i = 0; i < table.rows.length; i++) {
@@ -11,14 +45,25 @@ export const createTableFrame = (table: TableType): FrameNode => {
       frame.resize(frame.width, frame.height + row.height);
     }
     for (let j = 0; j < table.columns.length; j++) {
-      const rect = figma.createRectangle();
       const columnId = row.cells[j].columnId;
       const column = table.columns.find((column) => column.id === columnId);
-      rect.x = j * column.width;
-      rect.y = i * row.height;
-      rect.resize(column.width, row.height);
-      rect.fills = [{ type: 'SOLID', color: row.cells[j].backgroundColor }];
-      frame.appendChild(rect);
+      const cellRect = createCellRect(
+        row.cells[j],
+        column.width,
+        row.height,
+        j,
+        i
+      );
+      const cellText = await createCellText(
+        row.cells[j],
+        column.width,
+        row.height,
+        j,
+        i
+      );
+      const nodes: BaseNode[] = [cellRect, cellText];
+      const cellGroup = figma.group(nodes, figma.currentPage);
+      frame.appendChild(cellGroup);
       if (j === 0) {
         frame.resize(column.width, frame.height);
       } else {
